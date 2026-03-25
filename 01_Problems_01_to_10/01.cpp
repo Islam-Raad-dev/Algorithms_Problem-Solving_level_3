e3195b0a83c1a00e75b03c9137e2198047430235
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-03-25 17:40:31 +0300

    3 x 3 Random Matrix

--- 01_Problems_01_to_10/01.cpp
+++ 01_Problems_01_to_10/01.cpp
@@ -3,11 +3,42 @@
 3 x 3 Random Matrix
 
 */
-#include<iostream>
-#include<cstdlib>
+#include <iostream>
+#include <cstdlib>
+#include<ctime>
 using namespace std;
 
-int main(){
+int RandomNumber(int From, int To)
+{
+
+    int randNumb = rand() % (To - From + 1) + From;
+
+    return randNumb;
+}
+
+void FillMatrexWithRandomNumbers(int arr[3][3], short Rows, short Cols)
+{
+
+
+
+}
+
+void PritnMatrix(int arr[3][3], short Rows, short Cols){
+
+}
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[3][3];
+
+    FillMatrexWithRandomNumbers(arr, 3, 3);
+    
+    cout<<"\nThe Following is a 3x3 Random Matrix: \n";
+
+    PritnMatrix(arr, 3, 3);
+
+    system("pause>0");
 
     return 0;
 }
