c3f66f9ab1e1a057d07f175fcf00ccca0c039eba
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-03-28 20:57:42 +0300

    Sum Of Each Rom in 3 x 3 Random Matrix

--- 01_Problems_01_to_10/07.cpp
+++ 01_Problems_01_to_10/07.cpp
@@ -1,3 +1,97 @@
 /*
-  
+
+Sum Of Each Rom in 3 x 3 Random Matrix
+
 */
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+
+    int randNumb = rand() % (To - From + 1) + From;
+
+    return randNumb;
+}
+
+void FillMatrexWithRandomNumbers(int arr[3][3], short Rows, short Cols)
+{
+
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            arr[i][j] = RandomNumber(1, 100);
+        }
+    }
+}
+
+void PritnMatrix(int arr[3][3], short Rows, short Cols)
+{
+
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            cout << setw(3) << arr[i][j] << "    ";
+        }
+
+        cout << "\n";
+    }
+}
+
+int SumColms(int arr[3][3], short RowNumbers, short Cols)
+{
+
+    int Sum = 0;
+
+    for (short j = 0; j < Cols; j++)
+    {
+
+        Sum += arr[RowNumbers][j];
+    }
+
+    return Sum;
+}
+
+void PrintEachRowSum(int arr[3][3], short Rows, short Cols)
+{
+
+    for (short i = 0; i < Rows; i++)
+    {
+
+        cout << "Rows [" << i + 1 << "] Sum = " << SumRows(arr, i, Cols) << endl;
+    }
+}
+
+void SumMatrixColmsInArra(int arr[3][3], int arrSum[3], short Rows, short Cols)
+{
+
+    for (short i = 0; i < Rows; i++)
+    {
+        arrSum[i] = SumRows(arr, i, Cols);
+    }
+}
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[3][3];
+    int arrSum[3];
+
+    FillMatrexWithRandomNumbers(arr, 3, 3);
+    cout << "\n\nThe Following is a 3 x 3 Random Matrix: \n\n";
+    PritnMatrix(arr, 3, 3);
+
+    SumMatrixColmsInArray(arr, arrSum, 3, 3);
+
+    cout << "\nThe Following Are The Sum Of Each Colms In A Matrix: \n\n";
+    PrintEachRowSum(arr, 3, 3);
+
+    cout << "\n\n";
+
+    return 0;
+}
