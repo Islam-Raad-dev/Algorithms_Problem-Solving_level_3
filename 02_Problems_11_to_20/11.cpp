1e300fe672b8bd11e4de2a0a668a18a566309a89
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-03-29 12:18:54 +0300

    Add

---
+++ 02_Problems_11_to_20/11.cpp
@@ -0,0 +1,76 @@
+/*
+
+Print Sum Of Random Matrix
+
+*/
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+
+    int randNumb = rand() % (To - From + 1) + From;
+
+    return randNumb;
+}
+
+void FillMatrexWithRandomNumbers(int Matrix1[3][3], short Rows, short Cols)
+{
+
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            Matrix1[i][j] = RandomNumber(1, 100);
+        }
+    }
+}
+
+void PritnMatrix(int Matrix1[3][3], short Rows, short Cols)
+{
+
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            printf("%0*d     ", 2, Matrix1[i][j]);
+        }
+
+        cout << "\n";
+    }
+}
+
+int SumOfMatrix(int Matrix1[3][3], short Rows, short Cols)
+{
+
+    int Sum = 0;
+
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            Sum += Matrix1[i][j];
+        }
+    }
+
+    cout << "\n";
+    
+    return Sum;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int Matrix1[3][3];
+    
+    FillMatrexWithRandomNumbers(Matrix1, 3, 3);
+    
+    cout << "Matrix 1: \n\n";
+    PritnMatrix(Matrix1, 3, 3);
+
+    cout << "\nSum OF The Matrixs is: " << SumOfMatrix(Matrix1, 3, 3) << endl;
+    return 0;
+}
