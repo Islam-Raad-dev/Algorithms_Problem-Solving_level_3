9de4728f6530bb68b69859a47e0a18091af72188
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-03-29 10:55:52 +0300

    3 x 3 Oedered Matrix

--- 01_Problems_01_to_10/06.cpp
+++ 01_Problems_01_to_10/06.cpp
@@ -6,25 +6,19 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <iomanip>
 using namespace std;
-
-int RandomNumber(int From, int To)
-{
-
-    int randNumb = rand() % (To - From + 1) + From;
-
-    return randNumb;
-}
 
 void FillMatrexWithOrderNumbers(int arr[3][3], short Rows, short Cols)
 {
+
+    short Counter = 0;
 
     for (short i = 0; i < Rows; i++)
     {
         for (short j = 0; j < Cols; j++)
         {
-            arr[i][j] = RandomNumber(1, 100);
+            Counter++;
+            arr[i][j] = Counter;
         }
     }
 }
@@ -52,7 +46,7 @@ int main()
 
     FillMatrexWithOrderNumbers(arr, 3, 3);
 
-    cout << "\n\nThe Following is a 3 x 3 Random Matrix: \n\n";
+    cout << "\n\nThe Following is a 3 x 3 Ordered Matrix: \n\n";
 
     PritnMatrix(arr, 3, 3);
 
