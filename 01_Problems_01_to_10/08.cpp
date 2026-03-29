e99d47b077922e2803ce54b0251366de79345915
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-03-29 11:13:15 +0300

    Random 2 Matrix

--- 01_Problems_01_to_10/08.cpp
+++ 01_Problems_01_to_10/08.cpp
@@ -35,7 +35,7 @@ void PritnMatrix(int Matrix[3][3], short Rows, short Cols)
     {
         for (short j = 0; j < Cols; j++)
         {
-            cout <<Matrix[i][j] << "    ";
+            cout << Matrix[i][j] << "    ";
         }
 
         cout << "\n";
@@ -58,8 +58,15 @@ void PritnTransMatrix(int Matrix[3][3], short Rows, short Cols)
     cout << "\n";
 }
 
-void MultiplayMatrix(int Matrix1[3][3], int Matrix2[3][3], int MatrixResult[3][3], short Rows, short Cols){
-
+void MultiplayMatrix(int Matrix1[3][3], int Matrix2[3][3], int MatrixResult[3][3], short Rows, short Cols)
+{
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            MatrixResult[i][j] = Matrix1[i][j] * Matrix2[i][j];
+        }
+    }
 }
 
 int main()
@@ -68,29 +75,17 @@ int main()
 
     int Matrix1[3][3], Matrix2[3][3], MatrixResult[3][3];
 
-
     FillMatrexWithRandomNumbers(Matrix1, 3, 3);
-
     cout << "\n\nMatrix 1: \n\n";
-
     PritnMatrix(Matrix1, 3, 3);
 
-
-
     FillMatrexWithRandomNumbers(Matrix2, 3, 3);
-
     cout << "\n\nMatrix 2: \n\n";
-    
     PritnMatrix(Matrix2, 3, 3);
 
-
-
     MultiplayMatrix(Matrix1, Matrix2, MatrixResult, 3, 3);
-
     cout << "\n\nResults: \n\n";
-
     PritnMatrix(MatrixResult, 3, 3);
-    
 
     return 0;
 }
