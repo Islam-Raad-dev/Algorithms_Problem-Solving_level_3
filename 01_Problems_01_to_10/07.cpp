7b5be54588c3b1f9350e27821b3e3c6bb2aa5d47
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-03-29 11:00:45 +0300

    3 x 3 Transposed Matrix

--- 01_Problems_01_to_10/07.cpp
+++ 01_Problems_01_to_10/07.cpp
@@ -1,30 +1,24 @@
 /*
 
-Sum Of Each Rom in 3 x 3 Random Matrix
+3 x 3 Oedered Matrix
 
 */
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <iomanip>
 using namespace std;
 
-int RandomNumber(int From, int To)
+void FillMatrexWithOrderNumbers(int arr[3][3], short Rows, short Cols)
 {
 
-    int randNumb = rand() % (To - From + 1) + From;
-
-    return randNumb;
-}
-
-void FillMatrexWithRandomNumbers(int arr[3][3], short Rows, short Cols)
-{
+    short Counter = 0;
 
     for (short i = 0; i < Rows; i++)
     {
         for (short j = 0; j < Cols; j++)
         {
-            arr[i][j] = RandomNumber(1, 100);
+            Counter++;
+            arr[i][j] = Counter;
         }
     }
 }
@@ -36,45 +30,29 @@ void PritnMatrix(int arr[3][3], short Rows, short Cols)
     {
         for (short j = 0; j < Cols; j++)
         {
-            cout << setw(3) << arr[i][j] << "    ";
+            cout << arr[i][j] << "    ";
         }
 
         cout << "\n";
     }
 }
 
-int SumColms(int arr[3][3], short RowNumbers, short Cols)
-{
-
-    int Sum = 0;
-
-    for (short j = 0; j < Cols; j++)
-    {
-
-        Sum += arr[RowNumbers][j];
-    }
-
-    return Sum;
-}
-
-void PrintEachRowSum(int arr[3][3], short Rows, short Cols)
+void PritnTransMatrix(int arr[3][3], short Rows, short Cols)
 {
 
     for (short i = 0; i < Rows; i++)
     {
+        for (short j = 0; j < Cols; j++)
+        {
+            cout << arr[j][i] << "    ";
+        }
 
-        cout << "Rows [" << i + 1 << "] Sum = " << SumColms(arr, i, Cols) << endl;
+        cout << "\n";
     }
+
+    cout << "\n";
 }
 
-void SumMatrixColmsInArra(int arr[3][3], int arrSum[3], short Rows, short Cols)
-{
-
-    for (short i = 0; i < Rows; i++)
-    {
-        arrSum[i] = SumColms(arr, i, Cols);
-    }
-}
 int main()
 {
     srand((unsigned)time(NULL));
@@ -82,16 +60,13 @@ int main()
     int arr[3][3];
     int arrSum[3];
 
-    FillMatrexWithRandomNumbers(arr, 3, 3);
-    cout << "\n\nThe Following is a 3 x 3 Random Matrix: \n\n";
+    FillMatrexWithOrderNumbers(arr, 3, 3);
+
+    cout << "\n\nThe Following is a 3 x 3 Ordered Matrix: \n\n";
     PritnMatrix(arr, 3, 3);
 
-    SumMatrixColmsInArra(arr, arrSum, 3, 3);
-
-    cout << "\nThe Following Are The Sum Of Each Colms In A Matrix: \n\n";
-    PrintEachRowSum(arr, 3, 3);
-
-    cout << "\n\n";
+    cout << "\n\nThe Following is The Transposed Matrix: \n\n";
+    PritnTransMatrix(arr, 3, 3);
 
     return 0;
 }
