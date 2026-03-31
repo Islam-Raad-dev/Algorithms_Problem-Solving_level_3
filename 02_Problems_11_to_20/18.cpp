4e70acd2becab14d7c62a6daa92064e02ed3c854
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-03-31 12:01:39 +0300

    Intersected Numbber In Matrix

--- 02_Problems_11_to_20/18.cpp
+++ 02_Problems_11_to_20/18.cpp
@@ -39,25 +39,39 @@ bool IsNumberInMatrix(int Matrix1[3][3], int Number, short Rows, short Cols)
     return false;
 }
 
+void PrintIntersectedNumbberInMatrix(int Matrix1[3][3], int Matrix2[3][3], short Rows, short Cols)
+{
+
+    short Number;
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            Number = Matrix1[i][j];
+
+            if (IsNumberInMatrix(Matrix2, Number, Rows, Cols))
+                ;
+            {
+                cout << setw(3) << Number << "     ";
+            }
+        }
+    }
+}
 int main()
 {
     srand((unsigned)time(NULL));
 
-    int Matrix[3][3] = {{77, 5, 12}, {22, 20, 1}, {1, 0, 0}};
+    int Matrix1[3][3] = {{77, 5, 12}, {22, 20, 1}, {1, 0, 0}};
+    int Matrix2[3][3] = {{5, 80, 90}, {22, 77, 1}, {10, 8, 33}};
 
-    PritnMatrix(Matrix, 3, 3);
+    cout << "\nMatrix 1:\n";
+    PritnMatrix(Matrix1, 3, 3);
 
-    int Number;
-    cout << "\nEnter The Number To Count in Matrix: ";
-    cin >> Number;
+    cout << "\nMatrix 2:\n";
+    PritnMatrix(Matrix2, 3, 3);
 
-    if (IsNumberInMatrix(Matrix, Number, 3, 3))
-    {
-        cout << "\nYes, There Is.\n";
-    }
-    else
-    {
-        cout << "\nNo, There Is Not.\n";
-    }
+    cout << "\nIntersected Numbber Are: \n";
+    PrintIntersectedNumbberInMatrix(Matrix1, Matrix2, 3, 3);
+
     return 0;
 }
