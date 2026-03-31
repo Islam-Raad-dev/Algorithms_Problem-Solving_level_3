5b317367a04c42cccf83cba604bf126de2cbdf27
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-03-31 20:34:02 +0300

    Invert Charcter Case

---
+++ 03_Problems_21_to_30/27.cpp
@@ -0,0 +1,62 @@
+/*
+
+Invert Charcter Case
+
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+string ReadString()
+{
+
+    string S1;
+
+    cout << "Please Enter The String: ";
+    getline(cin, S1);
+
+    return S1;
+}
+string UpperCaseAllLetter(string S1)
+{
+
+    for (short i = 0; i < S1.length(); i++)
+    {
+
+        S1[i] = toupper(S1[i]);
+    }
+
+    return S1;
+}
+
+string LowerCaseAllLetter(string S1)
+{
+
+    for (short i = 0; i < S1.length(); i++)
+    {
+
+        S1[i] = tolower(S1[i]);
+    }
+
+    return S1;
+
+}
+int main()
+{
+
+    string S1 = ReadString();
+
+    cout << "\nString After Upper Case: \n";
+
+    S1 = UpperCaseAllLetter(S1);
+
+    cout << S1 << endl;
+
+    cout << "\nString After Lower Case: \n";
+
+    S1 = LowerCaseAllLetter(S1);
+
+    cout << S1 << endl;
+
+    return 0;
+}
