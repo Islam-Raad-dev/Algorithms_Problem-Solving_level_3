7185280d5a592ae13886bac94bd71280ad765e15
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-03-31 12:17:37 +0300

    Add

---
+++ 03_Problems_21_to_30/21.cpp
@@ -0,0 +1,61 @@
+/*
+
+Palindrome Matrix
+
+*/
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+void PritnMatrix(int Matrix[3][3], short Rows, short Cols)
+{
+
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            cout << setw(3) << Matrix[i][j] << "    ";
+        }
+
+        cout << "\n";
+    }
+}
+
+int IsPalindromeMatrix(int Matrix1[3][3], short Rows, short Cols)
+{
+
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            if (Matrix1[i][j] != Matrix1[j][Cols - 1 - j])
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int Matrix1[3][3] = {{77, 5, 12}, {22, 20, 1}, {1, 0, 0}};
+
+    cout << "\nMatrix :\n";
+    PritnMatrix(Matrix1, 3, 3);
+
+    if (IsPalindromeMatrix(Matrix1, 3, 3))
+    {
+        cout << "\nYes, The Matirx is Palindrom. \n";
+    }
+    else
+    {
+        cout << "\nNo, The Matirx is Not Palindrom. \n";
+    }
+
+    return 0;
+}
