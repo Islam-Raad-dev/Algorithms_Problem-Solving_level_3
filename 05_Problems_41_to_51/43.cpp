93a9e7a1b20511aeff35c3a93be9c96bcba9259a
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-01 17:07:38 +0300

    Edit

--- 05_Problems_41_to_51/43.cpp
+++ 05_Problems_41_to_51/43.cpp
@@ -5,17 +5,74 @@ Replace Word(Custom)
 */
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-string ReplaceWordInString(string S1, string ReplaceTo, string StringToReplace, bool Matchcase = true)
+vector<string> SplitString(string S1, string delim)
 {
-    short pos = S1.find(StringToReplace);
+    vector<string> vString;
 
-    while (pos != std::string::npos)
+    short pos = 0;
+    string sword;
+
+    while ((pos = S1.find(delim)) != std::string::npos)
     {
-        S1 = S1.replace(pos, StringToReplace.length(), ReplaceTo);
-        pos = S1.find(StringToReplace);
+        sword = S1.substr(0, pos);
+
+        if (sword != "")
+        {
+            vString.push_back(sword);
+        }
+
+        S1.erase(0, pos + delim.length());
+    }
+
+    if (S1 != "")
+    {
+        vString.push_back(S1);
+    }
+
+    return vString;
+}
+
+string JoinString(vector<string> vString, string delim)
+{
+    string S1;
+
+    for (string &s : vString)
+    {
+        S1 = S1 + s + delim;
+    }
+
+    return S1.substr(0, S1.length() - delim.length());
+}
+
+string LowerAllSplit(string S1)
+{
+}
+string ReplaceWordUsingSplit(string S1, string ReplaceTo, string StringToReplace, bool Matchcase = true)
+{
+    vector<string> vString = SplitString(S1, " ");
+
+    for (string &s : vString)
+    {
+
+        if (Matchcase)
+        {
+            if (s == StringToReplace)
+            {
+                s = ReplaceTo;
+            }
+        }
+
+        else
+        {
+            if (LowerAllSplit(s) == LowerAllSplit(StringToReplace))
+            {
+                s = ReplaceTo;
+            }
+        }
     }
 
     return S1;
@@ -27,10 +84,10 @@ int main()
     string ReplaceTo = "USA";
 
     cout << "\nString After Replace With Match Case: \n";
-    cout << ReplaceWordInString(S1, StringToReplace, ReplaceTo) << endl;
+    cout << ReplaceWordUsingSplit(S1, StringToReplace, ReplaceTo) << endl;
 
     cout << "\nString After Replace: \n";
-    cout << ReplaceWordInString(S1, StringToReplace, ReplaceTo, false);
+    cout << ReplaceWordUsingSplit(S1, StringToReplace, ReplaceTo, false);
 
     return 0;
 }
