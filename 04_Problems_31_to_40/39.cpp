308f5d331f557fe7bb7a56799c832c09e96a7650
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-01 15:34:01 +0300

    Edit

--- 04_Problems_31_to_40/39.cpp
+++ 04_Problems_31_to_40/39.cpp
@@ -5,41 +5,21 @@ Join String
 */
 #include <iostream>
 #include <string>
-#include<vector>
+#include <vector>
 using namespace std;
 
-
-vector<string> JoinString(string S1, string delim)
+string JoinString(vector<string> vString, string delim)
 {
-    vector<string> vString;
 
-    short pos = 0;
-    string sword;
-
-    while ((pos = S1.find(delim)) != std::string::npos)
-    {
-        sword = S1.substr(0, pos);
-
-        if (sword != "")
-        {
-            vString.push_back(sword);
-        }
-
-        S1.erase(0, pos + delim.length());
-    }
-
-    if (S1 != "")
-    {
-        vString.push_back(S1);
-    }
-
-    return vString;
 }
 
 int main()
 {
 
-    vector <string> vString;
+    vector<string> vString = {"Mohammed", "Faid", "Ali", "Islam"};
+
+    cout << "\n Vector After Join :\n";
+    cout << JoinString(vString, " ");
 
     return 0;
 }
