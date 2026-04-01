5945e64fd8cf9106bd825b7cb5972242158a8bd7
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-01 13:13:10 +0300

    Add

---
+++ 04_Problems_31_to_40/37.cpp
@@ -0,0 +1,58 @@
+/*
+
+Count Each Word in String
+
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+string ReadString()
+{
+
+    string S1;
+
+    cout << "Please Enter The String: ";
+    getline(cin, S1);
+
+    return S1;
+}
+
+short CountEachWordinString(string S1)
+{
+
+    string delim = " ";
+    short Counter = 0;
+    short pos = 0;
+    string sword;
+
+    while ((pos = S1.find(delim)) != std::string::npos)
+    {
+        sword = S1.substr(0, pos);
+
+        if (sword != "")
+        {
+            Counter++;
+        }
+
+        S1.erase(0, pos + delim.length());
+    }
+
+    if (S1 != "")
+    {
+        Counter++;
+    }
+
+    return Counter;
+}
+
+int main()
+{
+
+    string S1 = ReadString();
+
+    cout << "\nThe Number Of Each Word in Your String Is: ";
+    cout << CountEachWordinString(S1) << endl;
+
+    return 0;
+}
