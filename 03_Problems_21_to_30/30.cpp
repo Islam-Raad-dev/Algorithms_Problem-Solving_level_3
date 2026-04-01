7dfd1a7177b820e7f829492f0bf84f8da8c043a8
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-01 11:35:37 +0300

    Add

---
+++ 03_Problems_21_to_30/30.cpp
@@ -0,0 +1,58 @@
+/*
+
+Count Small / Capital Letters 
+
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+string ReadCharcter()
+{
+
+    string S1;
+
+    cout << "Please Enter The String: ";
+    getline(cin, S1);
+
+    return S1;
+}
+
+short CountSmallLetter(string S1)
+{
+    short Counter = 0;
+
+    for (short i = 0; i < S1.length(); i++)
+    {
+        if (islower(S1[i]))
+            Counter++;
+    }
+
+    return Counter;
+}
+
+short CountCapitalLetter(string S1)
+{
+    short Counter = 0;
+
+    for (short i = 0; i < S1.length(); i++)
+    {
+        if (isupper(S1[i]))
+            Counter++;
+    }
+
+    return Counter;
+}
+
+int main()
+{
+    string S1 = ReadCharcter();
+
+    cout << "\nString Length: " << S1.length();
+
+    cout << "\nCapital Letters Count: " << CountCapitalLetter(S1);
+
+    cout << "\nSmall Letters Count: " << CountSmallLetter(S1) << "\n";
+
+    return 0;
+}
