d382e15b0dab4198aa34ecf739abda7f7ee4b5d7
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-01 16:19:53 +0300

    Edit

--- 05_Problems_41_to_51/41.cpp
+++ 05_Problems_41_to_51/41.cpp
@@ -18,13 +18,22 @@ string ReadString()
     return S1;
 }
 
-string ReverseWord(){
+string ReverseWord(string S1)
+{
+    string Word;
+    
+    for(short i = 0; i < S1.length(); i++){
+        
+    }
 
+    return Word;
 }
 int main()
 {
     string S1 = ReadString();
-    
+
+    cout << "\nString After Reverse: \n";
+    cout << ReverseWord(S1) << endl;
 
     return 0;
 }
