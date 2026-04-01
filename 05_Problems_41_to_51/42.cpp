52fd50edf10da9804b60f54afa3075d285463f72
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-01 16:50:03 +0300

    Edit

--- 05_Problems_41_to_51/42.cpp
+++ 05_Problems_41_to_51/42.cpp
@@ -18,15 +18,17 @@ string ReadString()
 
     return S1;
 }
-string ReplaceWordInString(string S1)
+string ReplaceWordInString(string S1, string ReplaceTo, string StringToReplace)
 {
 }
 int main()
 {
     string S1 = ReadString();
+    string StringToReplace = "IRAQ";
+    string ReplaceTo = "USA";
 
     cout << "\nString After Replace: \n";
-    cout << ReplaceWordInString(S1) << endl;
+    cout << ReplaceWordInString(S1, StringToReplace, ReplaceTo) << endl;
 
     return 0;
 }
