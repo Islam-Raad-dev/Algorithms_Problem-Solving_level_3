0e193c049fc4afea246da057a9142da896525dc9
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-01 11:09:53 +0300

    Add

---
+++ 03_Problems_21_to_30/29.cpp
@@ -0,0 +1,48 @@
+/*
+
+Invert All Letter Case
+
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+string ReadCharcter()
+{
+
+    string S1;
+
+    cout << "Please Enter The String: ";
+    getline (cin ,S1);
+
+    return S1;
+}
+
+char InvertCharcterCase(char C1)
+{
+    return isupper(C1) ? tolower(C1) : toupper(C1);
+}
+
+string InvertAllLetterCase(string S1)
+{
+
+    for (short i = 0; i < S1.length(); i++)
+    {
+        S1[i] = InvertCharcterCase(S1[i]);
+    }
+
+    return S1;
+}
+
+int main()
+{
+    string S1 = ReadCharcter();
+
+    cout << "\nString Letter After Inverting All Letter: \n";
+
+    S1 = InvertAllLetterCase(S1);
+
+    cout << S1 << endl;
+
+    return 0;
+}
