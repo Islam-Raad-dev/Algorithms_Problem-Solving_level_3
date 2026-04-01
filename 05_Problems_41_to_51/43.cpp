c1ff72c1d83478336f857d58defb632ba19fb703
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-01 16:58:13 +0300

    Add

---
+++ 05_Problems_41_to_51/43.cpp
@@ -0,0 +1,33 @@
+/*
+
+Replace Word
+
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string ReplaceWordInString(string S1, string ReplaceTo, string StringToReplace)
+{
+    short pos = S1.find(StringToReplace);
+
+    while (pos != std::string::npos)
+    {
+        S1 = S1.replace(pos, StringToReplace.length(), ReplaceTo);
+        pos = S1.find(StringToReplace);
+    }
+
+    return S1; 
+}
+int main()
+{
+    string S1 = "Welcome To Iraq, Iraq Is A Nice Place.";
+    string StringToReplace = "Iraq";
+    string ReplaceTo = "USA";
+
+    cout << "\nString After Replace: \n";
+    cout << ReplaceWordInString(S1, StringToReplace, ReplaceTo) << endl;
+
+    return 0;
+}
