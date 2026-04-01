fc38b6d47cd55f02437cba6f5ef4d78c54129f72
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-01 15:48:40 +0300

    Join String (Overloading)

--- 04_Problems_31_to_40/40.cpp
+++ 04_Problems_31_to_40/40.cpp
@@ -20,13 +20,30 @@ string JoinString(vector<string> vString, string delim)
     return S1.substr(0, S1.length() - delim.length());
 }
 
+string JoinString(string arrString, short Length, string delim)
+{
+    string S1 = " ";
+
+    for (short i = 0; i < Length; i++)
+    {
+        S1 = S1 + arrString[i] + delim;
+    }
+
+    return S1.substr(0, S1.length() - delim.length());
+}
+
 int main()
 {
 
     vector<string> vString = {"Mohammed", "Faid", "Ali", "Islam"};
 
+    string arrString[] = {"Mohammed", "Faid", "Ali", "Islam"};
+
     cout << "\n Vector After Join :\n";
-    cout << JoinString(vString, "|*|") << endl;
+    cout << JoinString(vString, " ") << endl;
+
+    cout << "\n Array After Join :\n";
+    cout << JoinString(arrString, 4, " ") << endl;
 
     return 0;
 }
