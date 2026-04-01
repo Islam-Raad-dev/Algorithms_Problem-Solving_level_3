0c954bf94846384104180501e3cf0276e036b3e1
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-01 11:40:35 +0300

    Add

---
+++ 04_Problems_31_to_40/31.cpp
@@ -0,0 +1,45 @@
+/*
+
+Count Letters
+
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+string ReadCharcter()
+{
+
+    string S1;
+
+    cout << "Please Enter The String: ";
+    getline(cin, S1);
+
+    return S1;
+}
+
+short CountLetter(string S1, char C1)
+{
+    short Counter = 0;
+
+    for (short i = 0; i < S1.length(); i++)
+    {
+        if (S1[i] == C1)
+            Counter++;
+    }
+
+    return Counter;
+}
+
+int main()
+{
+    string S1 = ReadCharcter();
+    char C1;
+
+    cout << "Please Enter A Charcter: ";
+    cin >> C1;
+
+    cout << "Letter [" << C1 << "] Count = " << CountLetter(S1, C1) << endl;
+
+    return 0;
+}
