0157736796a1bad4c95ac737b1bf0ea83182a45e
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-01 17:20:23 +0300

    Edit

--- 05_Problems_41_to_51/44.cpp
+++ 05_Problems_41_to_51/44.cpp
@@ -9,92 +9,17 @@ Remove Punctuation
 
 using namespace std;
 
-vector<string> SplitString(string S1, string delim)
-{
-    vector<string> vString;
-
-    short pos = 0;
-    string sword;
-
-    while ((pos = S1.find(delim)) != std::string::npos)
-    {
-        sword = S1.substr(0, pos);
-
-        if (sword != "")
-        {
-            vString.push_back(sword);
-        }
-
-        S1.erase(0, pos + delim.length());
-    }
-
-    if (S1 != "")
-    {
-        vString.push_back(S1);
-    }
-
-    return vString;
-}
-
-string JoinString(vector<string> vString, string delim)
-{
-    string S1;
-
-    for (string &s : vString)
-    {
-        S1 = S1 + s + delim;
-    }
-
-    return S1.substr(0, S1.length() - delim.length());
-}
-
-string LowerAllSplit(string S1)
-{
-
-    for (short i = 0; i < S1.length(); i++)
-    {
-        S1[i] = tolower(S1[i]);
-    }
-
-    return S1;
-}
-string ReplaceWordUsingSplit(string S1, string ReplaceTo, string StringToReplace, bool Matchcase = true)
-{
-    vector<string> vString = SplitString(S1, " ");
-
-    for (string &s : vString)
-    {
-
-        if (Matchcase)
-        {
-            if (s == StringToReplace)
-            {
-                s = ReplaceTo;
-            }
-        }
-
-        else
-        {
-            if (LowerAllSplit(s) == LowerAllSplit(StringToReplace))
-            {
-                s = ReplaceTo;
-            }
-        }
-    }
-
-    return JoinString(vString, " ");
-}
-
 string RemovePunctuation(string S1)
 {
+    string S2;
 
-    return S1;
+    return S2;
 }
 int main()
 {
     string S1 = "Welcome To Iraq, Iraq Is A Nice Place.";
-    string StringToReplace = "Iraq";
-    string ReplaceTo = "USA";
+
+    cout << "\nOriginal String: \n" << S1;
 
     cout << "\nPunctuation Remove: \n";
     cout << RemovePunctuation(S1) << endl;
