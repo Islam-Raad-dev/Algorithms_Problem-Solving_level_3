8c0301af5fc2dc8d0622d6b9aefa9acb1078ade9
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-02 15:38:39 +0300

    Convert Line Data To Record

---
+++ 05_Problems_41_to_51/46.cpp
@@ -0,0 +1,72 @@
+/*
+
+Convert Line Data To Record
+
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct sClientInfo
+{
+
+    string AccountNumber;
+    string PinCode;
+    string FullName;
+    string PhoneNumber;
+    double AccountBalance;
+};
+
+sClientInfo ReadNewClient()
+{
+    sClientInfo Client;
+
+    cout << "Please Enter Client Data:\n";
+    cout << "-----------------------\n";
+
+    cout << "Enter Account Number: ";
+    getline(cin, Client.AccountNumber);
+
+    cout << "\nEnter Your PINCODE: ";
+    getline(cin, Client.PinCode);
+
+    cout << "\nEnter Your Full Name: ";
+    getline(cin, Client.FullName);
+
+    cout << "\nEnter Your Phone Number: ";
+    getline(cin, Client.PhoneNumber);
+
+    cout << "\nEnter Your Account Balance: ";
+    cin >> Client.AccountBalance;
+
+    return Client;
+}
+
+string ConvertRecordToLine(sClientInfo Clint, string Sepreator = "#//#")
+{
+    string stClientRecord = "";
+
+    stClientRecord += Clint.AccountNumber + Sepreator;
+    stClientRecord += Clint.PinCode + Sepreator;
+    stClientRecord += Clint.FullName + Sepreator;
+    stClientRecord += Clint.PhoneNumber + Sepreator;
+    stClientRecord += to_string(Clint.AccountBalance);
+
+    return stClientRecord;
+}
+
+int main()
+{
+
+    sClientInfo Client;
+    Client = ReadNewClient();
+
+    cout << "\nClient Record For Saving is: \n\n";
+
+    cout << ConvertRecordToLine(Client) << "\n"
+         << endl;
+
+    return 0;
+}
