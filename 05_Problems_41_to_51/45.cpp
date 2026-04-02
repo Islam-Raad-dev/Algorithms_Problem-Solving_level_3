d8fda51e79cc5c6a9b4af26abf6348dc53717769
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-02 15:16:25 +0300

    Convert Record To Line

--- 05_Problems_41_to_51/45.cpp
+++ 05_Problems_41_to_51/45.cpp
@@ -8,3 +8,43 @@ Convert Record To Line
 
 using namespace std;
 
+struct ClintInfo
+{
+
+    int AccountNumber;
+    short PinCode;
+    string FullName;
+    int PhoneNumber;
+    int AccountBalance;
+};
+
+int ReadClintInfo(ClintInfo Info)
+{
+
+    cout << "Enter Your Number: ";
+    cin >> Info.AccountNumber;
+
+    cout << "Enter Your PINCODE: ";
+    cin >> Info.PinCode;
+
+    cout << "Enter Your Full Name: ";
+    getline(cin, Info.FullName);
+
+    cout << "Enter Your Phone Number: ";
+    cin >> Info.PhoneNumber;
+
+    cout << "Enter Your Account Balance: ";
+    cin >> Info.AccountBalance;
+
+    return Info;
+}
+
+int main()
+{
+
+    ClintInfo Info;
+
+    cout << "\nClint Record For Saving is: \n";
+
+    return 0;
+}
