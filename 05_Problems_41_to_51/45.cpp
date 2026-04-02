2a27eff33d8265cb531585d7eb4511126d868002
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-02 15:06:24 +0300

    Edit

--- 05_Problems_41_to_51/45.cpp
+++ 05_Problems_41_to_51/45.cpp
@@ -8,29 +8,3 @@ Convert Record To Line
 
 using namespace std;
 
-string RemovePunctuation(string S1)
-{
-    string S2 = " ";
-
-    for (short i = 0; i < S1.length(); i++)
-    {
-        if (!ispunct(S1[i]))
-        {
-            S2 += S1[i];
-        }
-    }
-
-    return S2;
-}
-int main()
-{
-    string S1 = "Welcome To Iraq, Iraq Is A Nice Place.";
-
-    cout << "\nOriginal String: \n\n"
-         << S1;
-
-    cout << "\n\nPunctuation Remove: \n\n";
-    cout << RemovePunctuation(S1) << "\n" << endl;
-
-    return 0;
-}
