b6137bd11fa9c4cd5d51a115cd4940fda8d69154
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-03 17:02:37 +0300

    Edit

--- Projects/Project_1.cpp
+++ Projects/Project_1.cpp
@@ -16,60 +16,13 @@ using namespace std;
 
 const static string ClientsFileName = "Clients.txt";
 
-
-struct sClientInfo{};
-string ConvertRecordToLine(sClientInfo Clint, string Sepreator = "#//#");
-struct sClientInfo
-{
-
-    string AccountNumber;
-    string PinCode;
-    string FullName;
-    string PhoneNumber;
-    double AccountBalance;
-    bool MarkForDelete = false;
-};
-
-sClientInfo ReadNewClient()
-{
-    sClientInfo Client;
-
-    cout << "Please Enter Client Data:\n";
-    cout << "-----------------------\n";
-
-    cout << "Enter Account Number: ";
-    getline(cin, Client.AccountNumber);
-
-    cout << "\nEnter Your PINCODE: ";
-    getline(cin, Client.PinCode);
-
-    cout << "\nEnter Your Full Name: ";
-    getline(cin, Client.FullName);
-
-    cout << "\nEnter Your Phone Number: ";
-    getline(cin, Client.PhoneNumber);
-
-    cout << "\nEnter Your Account Balance: ";
-    cin >> Client.AccountBalance;
-
-    return Client;
+void MainMenueScreen(){
+    cout<<"=================================================================\n";
+    cout<<"\t\t\tMain Menue Screen";
+    cout<<"\n=================================================================\n";
 }
-
-string ConvertRecordToLine(sClientInfo Clint, string Sepreator = "#//#")
-{
-    string stClientRecord = "";
-
-    stClientRecord += Clint.AccountNumber + Sepreator;
-    stClientRecord += Clint.PinCode + Sepreator;
-    stClientRecord += Clint.FullName + Sepreator;
-    stClientRecord += Clint.PhoneNumber + Sepreator;
-    stClientRecord += to_string(Clint.AccountBalance);
-
-    return stClientRecord;
-}
-
 int main()
 {
-
+    MainMenueScreen();
     return 0;
 }
