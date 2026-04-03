d5f6f91bf8c85764fc7a1d30eb3a9219b0a07846
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-03 16:31:39 +0300

    Edit

--- Projects/Project_1.cpp
+++ Projects/Project_1.cpp
@@ -11,3 +11,53 @@ Bank 1:
 #include <iomanip>
 
 using namespace std;
+
+const static string ClientsFileName = "Clients.txt"; 
+
+struct sClientInfo
+{
+
+    string AccountNumber;
+    string PinCode;
+    string FullName;
+    string PhoneNumber;
+    double AccountBalance;
+};
+
+sClientInfo ReadNewClient()
+{
+    sClientInfo Client;
+
+    cout << "Please Enter Client Data:\n";
+    cout << "-----------------------\n";
+
+    cout << "Enter Account Number: ";
+    getline(cin, Client.AccountNumber);
+
+    cout << "\nEnter Your PINCODE: ";
+    getline(cin, Client.PinCode);
+
+    cout << "\nEnter Your Full Name: ";
+    getline(cin, Client.FullName);
+
+    cout << "\nEnter Your Phone Number: ";
+    getline(cin, Client.PhoneNumber);
+
+    cout << "\nEnter Your Account Balance: ";
+    cin >> Client.AccountBalance;
+
+    return Client;
+}
+
+string ConvertRecordToLine(sClientInfo Clint, string Sepreator = "#//#")
+{
+    string stClientRecord = "";
+
+    stClientRecord += Clint.AccountNumber + Sepreator;
+    stClientRecord += Clint.PinCode + Sepreator;
+    stClientRecord += Clint.FullName + Sepreator;
+    stClientRecord += Clint.PhoneNumber + Sepreator;
+    stClientRecord += to_string(Clint.AccountBalance);
+
+    return stClientRecord;
+}
