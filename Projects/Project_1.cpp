8ff8ea7d5a797605e21f60ad607208921f6b763b
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-05 10:27:10 +0300

    Edit

--- Projects/Project_1.cpp
+++ Projects/Project_1.cpp
@@ -16,11 +16,23 @@ using namespace std;
 
 const static string ClientsFileName = "Clients.txt";
 
+struct sClientInfo
+{
+    string AccountNumber;
+    string PinCode;
+    string FullName;
+    string PhoneNumber;
+    double AccountBalance;
+    bool MarkForDelete = false;
+};
+
 void MainMenueScreen(){
     cout<<"=================================================================\n";
     cout<<"\t\t\tMain Menue Screen";
     cout<<"\n=================================================================\n";
 }
+
+void 
 int main()
 {
     MainMenueScreen();
