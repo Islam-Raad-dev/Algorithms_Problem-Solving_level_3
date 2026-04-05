1f538a47b25c3d9c885c7985be447a0d173d868b
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-05 10:33:57 +0300

    Add

--- Projects/Project_1.cpp
+++ Projects/Project_1.cpp
@@ -16,6 +16,16 @@ using namespace std;
 
 const static string ClientsFileName = "Clients.txt";
 
+enum enMainMenuOptions
+{
+    eShow = 1,
+    eAdd = 2,
+    eDelete = 3,
+    eUpdate = 4,
+    eFind = 5,
+    eExit = 6
+};
+
 struct sClientInfo
 {
     string AccountNumber;
@@ -26,18 +36,20 @@ struct sClientInfo
     bool MarkForDelete = false;
 };
 
-void MainMenueScreen(){
+void MainMenueScreen()
+{
 
     system("clear");
-    cout<<"=================================================================\n";
-    cout<<"\t\t\tMain Menue Screen";
-    cout<<"\n=================================================================\n";
+    cout << "=================================================================\n";
+    cout << "\t\t\tMain Menue Screen";
+    cout << "\n=================================================================\n";
 
-    cout<<"\t[1] Show Client List.\n";
-    cout<<"\t[2] Add New Client.\n";
-    cout<<"\t[3] Delete Client.\n";
-    cout<<"\t[4] Update Client Info.\n";
-    cout<<"\t[5] Find Client.\n";
+    cout << "\t[1] Show Client List.\n";
+    cout << "\t[2] Add New Client.\n";
+    cout << "\t[3] Delete Client.\n";
+    cout << "\t[4] Update Client Info.\n";
+    cout << "\t[5] Find Client.\n";
+    cout << "\t[6] Exit.\n";
 }
 int main()
 {
